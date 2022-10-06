60d50e4d086a1cf4adae3c5071477e7616f6c905
bladeshape <tekigod@mail.ru>
2022-10-07 00:14:28 +0300

    asdas

--- Ex3.py
+++ Ex3.py
@@ -3,6 +3,6 @@
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-lst = list(map(float, input("Введите числа через пробел:").split()))
+lst = list(map(float, input('Введите числа через пробел: ').split()))
 new_lst = [round(i%1,2) for i in lst if i%1 != 0]
 print(max(new_lst) - min(new_lst))
